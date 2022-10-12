685149b0cd4bee60752a869cbefcc6a24740c702
Jampa Uchoa <joaopcu@gmail.com>
2022-10-12 01:58:44 -0300

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import { AppProps } from 'next/app'
 import '../styles/index.scss'
 import Layout from '../components/layout'
+import Hotjar from '@hotjar/browser';
+import ReactGA from "react-ga4";
+
+Hotjar.init(3197895, 6);
+ReactGA.initialize("G-W5SECKPKSP");
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
