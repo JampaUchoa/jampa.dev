eea085be276ba1ca477a9d1843322b436be26d25
Jampa Uchoa <joaopcu@gmail.com>
2023-01-31 01:18:43 -0300

    styling

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://jampa.dev',
-    generateRobotsTxt: true, // (optional)
+  siteUrl: process.env.SITE_URL || 'https://jampa.dev',
+  generateRobotsTxt: true // (optional)
 }
